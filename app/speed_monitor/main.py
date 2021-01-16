c9bcdb25324362ba2aa3b5a82f515da60314048d
Daniel Sharp <dsharpc@gmail.com>
2021-01-16 21:58:07 +0000

    Simple CLI interface

--- app/speed_monitor/main.py
+++ app/speed_monitor/main.py
@@ -1,9 +1,22 @@
+import sys
 from speed_monitor import measure_speed, extract_relevant_data
-from db.connector import insert_to_db
+from db.connector import insert_to_db, build_table
 from db.models import SpeedMeasurement
 
+class CommandNotValidException(Exception):
+    pass
+
+
 if __name__== "__main__":
-    res = measure_speed()
-    data = extract_relevant_data(res)
-    obj = SpeedMeasurement(**data)
-    insert_to_db(obj)
+    try:
+        if sys.argv[1] == 'measure':
+            res = measure_speed()
+            data = extract_relevant_data(res)
+            obj = SpeedMeasurement(**data)
+            insert_to_db(obj)
+        elif sys.argv[1] == 'build_table':
+            build_table()
+        else:
+            raise CommandNotValidException(f"Command <{sys.argv[1]}> not valid")
+    except IndexError:
+        print("Please make sure you give an argument and that the argument is valid")
